385aaced1514ddf41ff7391e321fd5cb7d39669b
krizi <krazy2krizi@gmail.com>
2012-08-14 21:44:25 +0200

    fix rangeparamterhandler

--- ParameterCheckHandler/src/main/java/ch/krizi/utility/parametercheck/handler/range/RangeParameterHandler.java
+++ ParameterCheckHandler/src/main/java/ch/krizi/utility/parametercheck/handler/range/RangeParameterHandler.java
@@ -3,10 +3,12 @@
  */
 package ch.krizi.utility.parametercheck.handler.range;
 
+import javax.inject.Named;
+import javax.inject.Singleton;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ch.krizi.utility.parametercheck.annotation.ParameterHandler;
 import ch.krizi.utility.parametercheck.exception.ParameterCheckException;
 import ch.krizi.utility.parametercheck.factory.MethodParameter;
 import ch.krizi.utility.parametercheck.handler.ParameterHandlerCheck;
@@ -15,10 +17,10 @@ import ch.krizi.utility.parametercheck.handler.ParameterHandlerCheck;
  * @author p105324
  * 
  */
-@ParameterHandler
+@Singleton
+@Named
 public class RangeParameterHandler implements ParameterHandlerCheck {
-	private final Logger logger = LoggerFactory
-			.getLogger(RangeParameterHandler.class);
+	private final Logger logger = LoggerFactory.getLogger(RangeParameterHandler.class);
 
 	@Override
 	public void check(MethodParameter methodParameter) {
@@ -29,17 +31,14 @@ public class RangeParameterHandler implements ParameterHandlerCheck {
 			Range range = methodParameter.getAnnotation(Range.class);
 
 			if (number.intValue() < range.min()) {
-				throw new ParameterCheckException(new IllegalArgumentException(
-						"parameter is to low"));
+				throw new ParameterCheckException(new IllegalArgumentException("parameter is to low"));
 			} else if (number.intValue() > range.max()) {
-				throw new ParameterCheckException(new IllegalArgumentException(
-						"parameter is to high"));
+				throw new ParameterCheckException(new IllegalArgumentException("parameter is to high"));
 			}
 
 		} catch (ClassCastException e) {
 			if (logger.isErrorEnabled()) {
-				logger.error("MethodParameter [" + methodParameter
-						+ "] unsupported", e);
+				logger.error("MethodParameter [" + methodParameter + "] unsupported", e);
 			}
 		}
 	}
